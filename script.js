4ff1b61d6578f4df9e81253f3f9297876d7f423f
noobieCreator737 <jutsuanime34@gmail.com>
2025-11-20 15:04:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let numbers = document.getElementById('number_option');
+
+let price = document.getElementById('price');
+
+// for (const element of numbers) {
+//     element.innerHTML = "#" + element.innerHTML * numbers.options[element.length - 1].value;
+// }
+
+let result = '';
+// for(var i = 0; i < numbers.length; i++){
+//     price.innerHTML = "#" + price.innerHTML * numbers.option[i].value;
+// }
+
+price.innerText = result;
+price.innerHTML = "#" + price.innerHTML * numbers.value;
